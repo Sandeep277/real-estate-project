387e19bcdbdc554e138925eef3e262b0de050515
Sandeep Kushwaha <102976365+Sandeep277@users.noreply.github.com>
2023-09-30 23:28:16 +0530

    mongoDB connection

---
+++ api/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+})
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('MongoDB connected...');
+}).catch(err => console.log(err));
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
