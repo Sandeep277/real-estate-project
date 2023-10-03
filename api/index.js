a63444e7007c6d06ab8e00640778dee6ee59b4f2
Sandeep Kushwaha <102976365+Sandeep277@users.noreply.github.com>
2023-10-03 22:00:55 +0530

    create listing api route

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import listingRoutes from './routes/listing.route.js';
 dotenv.config();
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(cookieParser());
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/listing", listingRoutes);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
