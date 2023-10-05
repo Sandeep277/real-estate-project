17c3b371145c4a9a3ba36a729f0cd8962671b1e6
Sandeep Kushwaha <102976365+Sandeep277@users.noreply.github.com>
2023-10-05 21:04:01 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
@@ -16,6 +17,13 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/listing", listingRoutes);
 
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal Server Error";
@@ -29,6 +37,7 @@ app.use((err, req, res, next) => {
 mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log('MongoDB connected...');
 }).catch(err => console.log(err));
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
